4dec2296f9b0a687433dfa0083764c80ab5c72d9
Aivo Antsman <antsman@users.noreply.github.com>
2018-04-25 16:46:28 +0300

    Mosquitto and Alpine versions from running container

---
+++ get-versions.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+test -n "$1" && (
+    echo MOSQUITTO_VERSION=\"$(docker exec -t $1 mosquitto -h | grep version | awk '{ print $3 }')\" | tee env.properties
+    echo ALPINE_VERSION=\"$(docker exec -t $1 cat /etc/alpine-release | tr -d '\r')\" | tee -a env.properties
+) || (
+    echo Give container name as parameter!
+    exit 1
+)
+
